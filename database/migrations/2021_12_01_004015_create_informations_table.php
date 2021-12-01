aa357707b1eb1aaaae63ff96d5c83e2a94b4bff4
oscardv18 <oscarddiazvelasquez@gmail.com>
2021-11-30 20:58:54 -0400

    creacion de la nueva migracion para la tabla de informations

---
+++ database/migrations/2021_12_01_004015_create_informations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('doc_id');
+            $table->string('picture', 255);
+            # Foreign key
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade')
+                  ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informations');
+    }
+}
